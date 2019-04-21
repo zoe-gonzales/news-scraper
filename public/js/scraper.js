286f235e0e70181558f3b97c3c46d8e16ef7e3e6
zoe-gonzales <zoegonzales03@gmail.com>
2019-04-20 21:01:52 -0600

    updates to display

--- public/js/scraper.js
+++ public/js/scraper.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
 
-    $.ajax("/scrape", {
-        method: "GET"
-    }).then(function(error, data){
-        if (error) throw error;
-        console.log(data);
-    });
-
-
 });
