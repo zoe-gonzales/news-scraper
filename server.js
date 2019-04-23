6d3fc0703d44805d23af96703b50a68a6031be76
zoe-gonzales <zoegonzales03@gmail.com>
2019-04-22 20:34:16 -0600

    scraping just headlines

--- server.js
+++ server.js
@@ -29,6 +29,14 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraperdb";
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
+// db.Article.deleteMany({})
+//   .then(function(result){
+//     console.log(result);
+//   })
+//   .catch(function(error){
+//     console.log(error);
+//   });
+
 // Rendering homepage
 app.get("/", function(req, res){
     res.render("index");
@@ -36,64 +44,71 @@ app.get("/", function(req, res){
 
 // Scraping and sending data
 app.get("/scrape", function(req, res){
-  axios.get("https://www.wired.com/").then(function(response) {
+  axios.get("https://www.wired.com/")
+    .then(function(response) {
 
       var $ = cheerio.load(response.data);
 
-      $("li.post-listing-list-item__post").each(function(i, element) {
-        var title = $(element).find("h5").text();
-        var source = $(element).find("a").attr("href");
-        var byline = $(element).find("span.byline-component__content").text();
-        var section = $(element).find("span.post-listing-list-item__byline").text();
-        var thumbnail = $(element).find("img").attr("src");
+      $("ul").each(function(i, element) {
+        var title = $(this).find("h2").text().trim();
+        var source = $(this).find("a").attr("href");
+        var byline = $(this).find("a.byline-component__link").text();
+        var section = $(this).find("div.brow-component").text();
+        var thumbnail = $(this).find("img").attr("src");
 
-        // take scraped data and compare to data in db to determine if it already exists.
-        // Searching db for document that matches the title of the currently scraped one
-        db.Article.findOne({"title":title})
-          .then(function(data){
-            // if there's not duplicate document
-            if (!data){
-              // the article is added to the db
-              db.Article.create({
-                title: title,
-                source: source,
-                byline: byline,
-                section: section,
-                thumbnail: thumbnail
-              })
-              .then(function(articleData){
-                console.log(articleData);
-                res.sendStatus(200);
-                return;
-              })
-              .catch(function(error){
-                console.log(error);
-                res.sendStatus(500);
-                return;
-              });
-            }
-          })
-          .catch(function(error){
-            console.log(error);
-            res.status(500);
-            return;
-          });
+        if (title && source && byline && section && thumbnail){
+          // take scraped data and compare to data in db to determine if it already exists.
+          // Searching db for document that matches the title of the currently scraped one
+          db.Article.findOne({title:title})
+            .then(function(data){
+              // if there's not duplicate document
+              if (!data){
+                // the article is added to the db
+                db.Article.create({
+                  title: title,
+                  source: source,
+                  byline: byline,
+                  section: section,
+                  thumbnail: thumbnail
+                })
+                .then(function(articleData){
+                  console.log(articleData);
+                  return;
+                })
+                .catch(function(error){
+                  console.log(error);
+                  res.sendStatus(500);
+                  return;
+                });
+              }
+            })
+            .catch(function(error){
+              console.log(error);
+              res.status(500);
+              return;
+            });
+        }
       });
 
-      db.Article.find({})
-        .populate("comments")
-        .then(function(data){
-          var articles = data.slice(0, 50);
-          console.log(articles[0]);
-          res.render("index", {articles: articles});
-        })
-        .catch(function(error){
-          console.log(error);
-          res.sendStatus(500);
-          return;
-        });
-  });
+      setTimeout(function(){
+        res.redirect("/articles");
+      }, 500);
+      
+    });
 });
+
+app.get("/articles", function(req, res){
+  db.Article.find({})
+  .populate("comments")
+  .then(function(data){
+    res.render("index", {articles: data});
+  })
+  .catch(function(error){
+    console.log(error);
+    res.sendStatus(500);
+    return;
+  });
+})
 
 // Adding comments
 app.post("/comment/:id", function(req, res){
